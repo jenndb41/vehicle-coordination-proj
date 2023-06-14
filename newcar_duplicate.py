7d1c13e5729445d62bd4eba63c7c78ab8eff2e0b
jenndb41 <136380834+jenndb41@users.noreply.github.com>
2023-06-13 17:31:32 -0700

    Update newcar_duplicate.py

--- newcar_duplicate.py
+++ newcar_duplicate.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import pygame
+import stable_baselines3
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -44,7 +45,7 @@ class CarEnv(gym.Env):
         elif action_car_2 == 2:
             self.car_2.up()
         else:
-            self.car_2.down()  # Stop
+            self.car_2.down()
 
         self.car.update()
         self.car_2.update()
@@ -56,10 +57,12 @@ class CarEnv(gym.Env):
         reward_car_2 = self.car_2.get_reward()
         obs_car_2 = self.car_2.get_grid_cell()
 
-        pygame.event.pump()  # Process Pygame events
+        pygame.event.pump()
 
-        return obs, reward, done, {}
-        return obs_car_2, reward_car_2, done_car_2, {}
+        done_list = [done, done_car_2]
+        obs_list = [obs, obs_car_2]
+        reward_list = [reward, reward_car_2]
+        return done_list, obs_list, reward_list, {}
 
     def reset(self):
         self.car = Car()
@@ -80,12 +83,91 @@ class CarEnv(gym.Env):
         pygame.quit()
 
     def train(self):
+        # assign variables
         reward = 0
         x, y = self.car.position[0], self.car.position[1]
-        if self.game_map.get_at((int(x), int(y))) != (0, 255, 0):
+        reward_car_2 = 0
+        x_2, y_2 = self.car_2.position[0], self.car_2.position[1]
+        cars = []
+        cars = cars.append(self.car)
+        cars = cars.append(self.car_2)
+
+        # punish cars for touching white area
+        if self.game_map.get_at((int(x), int(y))) == (255, 255, 255):
             reward -= self.car.get_reward()
 
-        if self.game_map.get_at((int(x), int(y))) == (0, 255, 0):
+        # punish cars for colliding/being in same position
+        if self.car.position == self.car_2.position:
+            reward -= self.car.get_reward()
+            reward_car_2 -= self.car_2.get_reward()
+
+        # define bypass area
+        upper_bypass_area = [(2, 1)] + [(x, 0) for x in range(2, 8)] + [(8, 1)]
+        lower_bypass_area = [(2, 9)] + [(x, 9) for x in range(2, 8)] + [(8, 9)]
+
+        # checking distance of car from two openings to upper bypasses
+        distance_to_bypass1 = abs(self.car.position[0] - 2) + abs(self.car.position[1] - 1)
+        distance_to_bypass2 = abs(self.car.position[0] - 8) + abs(self.car.position[1] - 1)
+
+        # checking distance of car from two openings to lower bypasses
+        distance_to_bypass1 = abs(self.car.position[0] - 2) + abs(self.car.position[1] - 9)
+        distance_to_bypass2 = abs(self.car.position[0] - 8) + abs(self.car.position[1] - 9)
+
+        # checking distance between cars
+        distance_between_cars = abs(self.car.position[0] - self.car_2.position[0]) + abs(self.car.position[1] - self.car_2.position[1])
+
+        # rewarding either car for going to upper bypass if too close
+        if self.game_map.get_at((int(x), int(y))) == (0, 255, 0) and distance_between_cars <= 2 and distance_to_bypass1 <= 2:
+            if self.car.position[1] < self.car_2.position[1]:
+                if self.car.up() and any((int(x), int(y)) == coord for coord in upper_bypass_area):
+                    reward += self.car.get_reward()
+                else:
+                    if self.car_2.up() and any((int(x_2), int(y_2)) == coord for coord in upper_bypass_area):
+                        reward_car_2 += self.car.get_reward()
+        elif self.game_map.get_at((int(x), int(y))) == (0, 255, 0) and distance_between_cars <= 2 and distance_to_bypass2 <= 2:
+            if self.car.position[1] < self.car_2.position[1]:
+                if self.car.up() and any((int(x), int(y)) == coord for coord in upper_bypass_area):
+                    reward += self.car.get_reward()
+                else:
+                    if self.car_2.up() and any((int(x_2), int(y_2)) == coord for coord in upper_bypass_area):
+                        reward_car_2 += self.car.get_reward()
+
+        # rewarding either car for going to lower bypass if too close
+        if self.game_map.get_at((int(x), int(y))) == (0, 255, 0) and distance_between_cars <= 2 and distance_to_bypass3 <= 2:
+            if self.car.position[1] > self.car_2.position[1]:
+                if self.car.down() and any((int(x), int(y)) == coord for coord in lower_bypass_area):
+                    reward += self.car.get_reward()
+                else:
+                    if self.car_2.down() and any((int(x_2), int(y_2)) == coord for coord in lower_bypass_area):
+                        reward_car_2 += self.car.get_reward()
+        elif self.game_map.get_at((int(x), int(y))) == (0, 255, 0) and distance_between_cars <= 2 and distance_to_bypass4 <= 2:
+            if self.car.position[1] > self.car_2.position[1]:
+                if self.car.down() and any((int(x), int(y)) == coord for coord in lower_bypass_area):
+                    reward += self.car.get_reward()
+                else:
+                    if self.car_2.down() and any((int(x_2), int(y_2)) == coord for coord in lower_bypass_area):
+                        reward_car_2 += self.car.get_reward()
+        # avoiding collision when cars are not near bypass (horizontal section only)
+        elif distance_between_cars <= 2 and distance_to_bypass3 > 2 or distance_between_cars <= 2 and distance_to_bypass4 > 2:
+            for i in cars:
+                if self.game_map.get_at((int(cars[i].position[0]), int(cars[i].position[1]))) == (0, 255, 0):
+                    if 3 <= cars[i].position[0] <= 7 and self.game_map.get_at((int(cars[i].position[0]), int(cars[i].position[1] + 1))) == (0, 255, 0):
+                        # might add to above if statement "and if cars[i].position[1] < cars[i + 1].position[1]""
+                        while abs(cars[i + 1].position[0] - cars[i].position[0]) <= 2:
+                            if cars[i].position == cars[i].position:
+                                reward += cars[i].get_reward()
+                            elif cars[i + 1].position[1] + 1 == cars[i].position[1]:
+                                reward += cars[i + 1].get_reward()
+                    elif 3 <= cars[i].position[0] <= 7 and self.game_map.get_at((int(cars[i].position[0]), int(cars[i].position[1] - 1))) == (0, 255, 0):
+                        # might add to above if statement "and if cars[i].position[1] > cars[i + 1].position[1]""
+                        while abs(cars[i + 1].position[0] - cars[i].position[0]) <= 2:
+                            if cars[i].position == cars[i].position:
+                                reward += cars[i].get_reward()
+                            elif cars[i + 1].position[1] - 1 == cars[i].position[1]:
+                                reward += cars[i + 1].get_reward()
+
+       # checking that red car goes clockwise
+        if self.game_map.get_at((int(x), int(y))) != (255, 255, 255):
             if 2 <= self.car.position[1] <= 8:
                 if self.car.position[0] == 1:
                     if 4 <= self.car.position[1] <= 8:
@@ -143,9 +225,7 @@ class CarEnv(gym.Env):
         else:
             reward -= self.car.get_reward()
 
-        reward_car_2 = 0
-        x_2, y_2 = self.car_2.position[0], self.car_2.position[1]
-
+       # checking that black car goes counterclockwise
         if self.game_map.get_at((int(x_2), int(y_2))) == (0, 255, 0):
             if 2 <= self.car_2.position[1] <= 8:
                 if self.car_2.position[0] == 1:
@@ -272,11 +352,11 @@ class Car2:
         cell_y = self.position[1] * CELL_SIZE
         pygame.draw.rect(screen, (0, 0, 0), (cell_x, cell_y, 140, 80))
 
-env = CarEnv()
+'''env = CarEnv()
 obs = env.reset()
-
+'''
 done = False
-for _ in range(1000):
+'''for _ in range(1000):
     action = env.action_space.sample()
     action_car_2 = env.action_space_2.sample()
     obs, reward, done, _ = env.step(action, action_car_2)
@@ -284,3 +364,4 @@ for _ in range(1000):
     env.render()
 
 env.close()
+'''
