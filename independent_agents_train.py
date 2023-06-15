686f58560e78b8ff5519068f5b03066434bbaebf
jenndb41 <136380834+jenndb41@users.noreply.github.com>
2023-06-15 03:31:06 -0700

    Add files via upload

---
+++ independent_agents_train.py
@@ -0,0 +1,14 @@
+from ray import tune
+from ray.rllib.algorithms.ppo import PPO
+from git_env import *
+
+tune.run(
+    PPO,
+    stop={"episode_reward_mean": 200},
+    config={
+        "env": MultiAgentEnv,
+        "lr": 0.001,
+        "clip_param": 0.2,
+    },
+    num_samples=1
+)
