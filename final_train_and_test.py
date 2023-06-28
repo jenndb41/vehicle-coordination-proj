57be68b847b6709cd9345d9b093854f72b4343c9
jenndb41 <136380834+jenndb41@users.noreply.github.com>
2023-06-28 10:44:26 -0700

    Update final_train_and_test.py

--- final_train_and_test.py
+++ final_train_and_test.py
@@ -1,44 +1,30 @@
 import ray
-from ray.rllib.algorithms.ppo import PPO
+from ray.rllib.algorithms.ppo import PPOConfig
 from final_env import *
+from ray import tune
 import pygame
 import time
 
-
 ray.init()
 env = MultiAgentCars()
-checkpoint_path = "/Users/jennyjung/Downloads/multi_agent_final/algo_ind_agents/checkpoint_000000"
 
-config = {
-    "multiagent": {
-        "policies": {
-            "policy_1": (None, env.observation_space, env.action_space, {}),
-            "policy_2": (None, env.observation_space, env.action_space, {}),
+config = PPOConfig()\
+    .environment(env=MultiAgentCars)\
+    .multi_agent(
+        policies={
+            "policy_1": (
+                None, env.observation_space, env.action_space, {"gamma": 0.80}
+            ),
+            "policy_2": (
+                None, env.observation_space, env.action_space, {"gamma": 0.95}
+            ),
         },
-        "policy_mapping_fn": lambda agent_id: f"policy_{agent_id}",
-    },
-    "framework": "torch",
-    "num_workers": 1,
-}
+        policy_mapping_fn = lambda agent_id: f"policy_{agent_id}",
+    )\
+    .rollouts(num_rollout_workers=2)
 
-trainer = PPO(config=config, env=MultiAgentCars)
-trainer.restore(checkpoint_path)
+algo = config.build()
 
-done = {"__all__": False}
-observation = env.reset()
-
-while not done["__all__"]:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done["__all__"] = True
-
-    actions = {}
-    for agent_id in observation.keys():
-        action = trainer.compute_single_action(observation[agent_id], policy_id=f"policy_{agent_id}")
-        actions[agent_id] = action
-
-    observation, reward, done, _ = env.step(actions)  
-    env.render()
-    time.sleep(0.1)
-
-pygame.quit()
+for i in range(10):
+    results = algo.train()
+    print(results)
